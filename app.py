0648dcfae76f5f8c6572436f805c48515965f0c2
Exwhybaba <seyeoyelayo@gmail.com>
2026-01-14 03:05:27 +0100

    update app again

--- app.py
+++ app.py
@@ -1399,3 +1399,4 @@ if __name__ == "__main__":
         port = int(os.environ.get("PORT", 8050))
         app.run(debug=True, host='0.0.0.0', port=port)
         # app.run(debug=True, port=port)
+    
